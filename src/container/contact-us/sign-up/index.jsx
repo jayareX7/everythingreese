c8026cb250040abb8d8bb1af1c392c83ae2c1c44
Admin <jayarex07@gmail.com>
2023-01-22 21:16:14 -0500

    added mailchimp to landing page

--- src/container/contact-us/sign-up/index.jsx
+++ src/container/contact-us/sign-up/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { StaticImage } from "gatsby-plugin-image";
 import Button from "../../../components/shared/button";
 import SectionTitle from "../../../components/title";
+import SubscribeForm from "./mailchimp";
 
 const SignUpArea = ({ data }) => {
     return (
@@ -22,38 +23,7 @@ const SignUpArea = ({ data }) => {
                     aliqua. A arcu cursus vitae congue mauris rhoncus. Est
                     ultricies integer quis auctor elit sed vulputate mi.
                 </p>
-                <div className="grid gap-x-8 grid-cols-2">
-                    <div className="single-fild">
-                        <input
-                            className="px-6 h-14 mb-6 border-secondary-90 bg-secondary-100 hover:border-primary transition-all border-2 border-solid block rounded-md w-full focus:outline-none"
-                            type="text"
-                            placeholder="Name"
-                        />
-                    </div>
-                    <div className="single-fild">
-                        <input
-                            className="px-6 h-14 mb-6 border-secondary-90 bg-secondary-100 hover:border-primary transition-all border-2 border-solid block rounded-md w-full focus:outline-none"
-                            type="email"
-                            placeholder="E-mail"
-                        />
-                    </div>
-                </div>
-                {data?.buttons &&
-                    data.buttons.map(({ id, content, ...props }) => (
-                        <div
-                            className="section-title-warp text-center mt-10"
-                            key={id}
-                        >
-                            <Button {...props} className="text-white mb-10">
-                                {content}
-                                <StaticImage
-                                    className="align-middle ml-3 transition-all group-hover:ml-5"
-                                    src="../../../data/images/icons/arrrow-icon.webp"
-                                    alt=""
-                                />
-                            </Button>
-                        </div>
-                    ))}
+                <SubscribeForm />
             </div>
         </section>
     );
